10baee78ca32bf27a0ff945f7a3c1f11f93ede08
kashish shrivastav <en.shrivastavkashish@gmail.com>
2025-08-26 02:52:56 +0530

    bip

--- DataBase.php
+++ DataBase.php
@@ -4,73 +4,52 @@ require "DataBaseConfig.php";
 class DataBase
 {
     public $connect;
-    public $data;
-    private $sql;
-    protected $servername;
-    protected $username;
-    protected $password;
-    protected $databasename;
+    private $conn_string;
 
     public function __construct()
     {
-        $this->connect = null;
-        $this->data = null;
-        $this->sql = null;
         $dbc = new DataBaseConfig();
-        $this->servername = $dbc->servername;
-        $this->username = $dbc->username;
-        $this->password = $dbc->password;
-        $this->databasename = $dbc->databasename;
+        $this->conn_string = $dbc->conn_string;
     }
 
     function dbConnect()
     {
-        $this->connect = mysqli_connect($this->servername, $this->username, $this->password, $this->databasename);
+        $this->connect = pg_connect($this->conn_string);
         return $this->connect;
     }
 
     function prepareData($data)
     {
-        return mysqli_real_escape_string($this->connect, stripslashes(htmlspecialchars($data)));
+        return pg_escape_string($this->connect, htmlspecialchars($data));
     }
 
     function logIn($table, $username, $password)
     {
         $username = $this->prepareData($username);
-        $password = $this->prepareData($password);
-        $this->sql = "select * from " . $table . " where username = '" . $username . "'";
-        $result = mysqli_query($this->connect, $this->sql);
-        $row = mysqli_fetch_assoc($result);
-        if (mysqli_num_rows($result) != 0) {
+        $query = "SELECT * FROM $table WHERE username = '$username'";
+        $result = pg_query($this->connect, $query);
+        $row = pg_fetch_assoc($result);
+
+        if ($row) {
             $dbusername = $row['username'];
             $dbpassword = $row['password'];
             if ($dbusername == $username && password_verify($password, $dbpassword)) {
-                $login = true;
-            } else $login = false;
-        } else $login = false;
-        if ($dbusername == $username && password_verify($password, $dbpassword)) {
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['fullname'] = $row['fullname'];
-            $_SESSION['email'] = $row['email'];
-            $login = true;
+                return true;
+            }
         }
-        return $login;
+        return false;
     }
 
     function signUp($table, $fullname, $email, $username, $password)
     {
         $fullname = $this->prepareData($fullname);
         $username = $this->prepareData($username);
-        $password = $this->prepareData($password);
         $email = $this->prepareData($email);
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $this->sql =
-            "INSERT INTO " . $table . " (fullname, username, password, email) VALUES ('" . $fullname . "','" . $username . "','" . $password . "','" . $email . "')";
-        if (mysqli_query($this->connect, $this->sql)) {
-            return true;
-        } else return false;
+        $password = password_hash($this->prepareData($password), PASSWORD_DEFAULT);
+
+        $query = "INSERT INTO $table (fullname, username, password, email) 
+                  VALUES ('$fullname', '$username', '$password', '$email')";
+        return pg_query($this->connect, $query) ? true : false;
     }
-
 }
-
 ?>
--- DataBaseConfig.php
+++ DataBaseConfig.php
@@ -1,21 +1,12 @@
 <?php
-
 class DataBaseConfig
 {
-    public $servername;
-    public $username;
-    public $password;
-    public $databasename;
+    public $conn_string;
 
     public function __construct()
     {
-
-        $this->servername = 'localhost';
-        $this->username = 'root';
-        $this->password = 'K@shish420';
-        $this->databasename = 'loginSample';
-
+        // Use the Internal URL for apps running on Render (faster & secure)
+        $this->conn_string = "postgresql://loginsample_user:Y8SRQWascB2kZpHfq4gIX1Hnpcdt7TH0@dpg-d2mbva1r0fns73d03840-a/loginsample";
     }
 }
-
 ?>
